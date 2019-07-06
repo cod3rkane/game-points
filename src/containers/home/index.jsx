0dcf6e1c510218df2f2579d04db6fbc93e84df0d
cod3rkane <jcesar.developer@gmail.com>
2019-07-06 10:28:20 +0200

    improve styles

--- src/containers/home/index.jsx
+++ src/containers/home/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import './App.scss';
 import logo from './logo.svg';
 
 export const Home = () => {
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
-import './index.scss';
+import './styles/index.scss';
+
 import * as serviceWorker from './serviceWorker';
 import Routes from './routes';
 import Store from './reducers';
