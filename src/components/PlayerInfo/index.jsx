9cb1ff0beb196eef0dccf19ac492a16374504d06
cod3rkane <jcesar.developer@gmail.com>
2019-07-07 14:55:52 +0200

    trying new design

--- src/components/PlayerInfo/index.jsx
+++ src/components/PlayerInfo/index.jsx
@@ -7,9 +7,20 @@ import { collectItem } from '../../reducers/actions';
 export const PlayerInfor = ({ Inventory }) => {
   return (
     <div className="playerInfor">
-      <div>
-        <h2>Player Items</h2>
-      </div>
+      <section className="score-resume"></section>
+      <section className="score-bonus flex">
+        <h4>Bonuses</h4>
+        <span className="bonus">999</span>
+      </section>
+      <section className="score-total">
+        <div className="flex">
+          <h4>Total</h4>
+          <span className="score">999</span>
+        </div>
+        <div className="actions flex">
+          <button>New Game</button>
+        </div>
+      </section>
     </div>
   );
 };
